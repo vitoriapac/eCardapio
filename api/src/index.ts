1f5764a954b3e0b733c5645a6e3b3ca0989bc4f3
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-11-15 17:49:33 -0300

    Create models

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://vitoriapac:a1b2c3d4@cluster0.g8brn8g.mongodb.net/test"
+  )
+  .then(() => console.log("Connect on Mongo ✅"))
+  .catch(() => console.log("error on Mongo ❌"));
 
 app.listen(3333, () => {
   console.log("🚀 Server listening on http://localhost:3333");
