97390d012dd0287f846615009ec40fad0403b886
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-11-16 22:11:41 -0300

    Cancel Order

--- api/src/router.ts
+++ api/src/router.ts
@@ -11,6 +11,7 @@ import { listProductsByCategory } from "./app/useCases/categories/listProductsBy
 import { listOrders } from "./app/useCases/orders/listOrders";
 import { createOrder } from "./app/useCases/orders/createOrder";
 import { changeOrderStatus } from "./app/useCases/orders/changeOrderStatus";
+import { cancelOrder } from "./app/useCases/orders/cancelOrder";
 
 export const router = Router();
 
@@ -50,6 +51,4 @@ router.post("/orders", createOrder);
 router.patch("/orders/:orderId", changeOrderStatus);
 
 //Delete/cancel order
-router.delete("/orders/:orderId", (req, res) => {
-  res.send("🆗 Cancel Order");
-});
+router.delete("/orders/:orderId", cancelOrder);
